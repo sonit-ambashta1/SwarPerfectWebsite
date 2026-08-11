6297b5be4ce12fb9c2a083e032f1d7aa9c3ad1c4
sonitambashta <sunnyambashta@gmail.com>
2026-08-11 14:51:26 -0400

    Frontend workflow done

--- swar-perfect-website/src/pages/Dashboard.jsx
+++ swar-perfect-website/src/pages/Dashboard.jsx
@@ -7,7 +7,9 @@ const Dashboard = () => {
     const [videos, setVideos] = useState([]);
 
     async function fetchVideos(){
-        let video_list = await fetch("/videos.json").then((response) => {
+        const link = import.meta.env.VITE_CLOUDFRONT_DISTRIBUTION_LINK || "/videos.json";
+
+        let video_list = await fetch(link).then((response) => {
             if (!response.ok) {
                 throw new Error("Network response was not ok");
             }
